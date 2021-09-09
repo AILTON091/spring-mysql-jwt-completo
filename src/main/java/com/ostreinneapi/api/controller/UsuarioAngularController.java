58de6096e6768baa728f7f3fda214d2efba09762
Ailton da Costa <ailtondacosta-91@hotmail.com>
2021-09-09 18:26:52 -0300

    ajuste de crossOrigin usuarios

--- src/main/java/com/ostreinneapi/api/controller/UsuarioAngularController.java
+++ src/main/java/com/ostreinneapi/api/controller/UsuarioAngularController.java
@@ -4,6 +4,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import com.ostreinneapi.domian.model.UsuarioAngular;
 import com.ostreinneapi.domian.repository.UsuarioAngularRepository;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/api/usuarios")
 public class UsuarioAngularController {
 
